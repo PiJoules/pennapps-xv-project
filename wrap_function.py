5d15c8a927c8fa8e43141e5c6114cd1471bb1bdc
pijoules <lchan1994@yahoo.com>
2017-01-21 15:42:12 -0500

    gotta increase that github score

--- wrap_function.py
+++ wrap_function.py
@@ -1,12 +1,13 @@
 from __future__ import print_function
 
 import json
+import cv2
 
 from convert2d import getBinaryEdges
 from knn import knn
 from original_2d_convertion import convert_original_2D
 
-filename = "training2/image80.bag.jpg"
+filename = "training2/image1.pikachu.jpg"
 directory_path = "training2"
 
 training=convert_original_2D(directory_path)  # dict[str, arr]
@@ -15,7 +16,14 @@ print("training data:", len(training))
 # Convert mapping to lst
 training_vec = [(k, v) for k, v in training.iteritems()]
 
-test=getBinaryEdges(filename)  # 2d arr
+img = cv2.imread(filename, 0)
+assert img is not None, "Error imreading file '{}'".format(filename)
+num_cols = len(img[0])
+for i in xrange(len(img)):
+    for j in xrange(num_cols):
+        if img[i, j] > 0:
+            img[i, j] = 1
+test = img
 
 results = knn(map(lambda x: x[1], training_vec), test, best_n=10)  # list[tuple[int, 2d arr]]
 
