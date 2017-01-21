43c1150bcf43eaefca28b9162721f18a1f5ce014
Yash Mittal <yashmittal2009@gmail.com>
2017-01-21 01:45:14 -0500

    Converted to Py 2D list

--- edge.py
+++ edge.py
@@ -8,8 +8,9 @@ def getBinaryEdges(filename):
     edges = cv2.Canny(img, 100, 200)
     edges_binary = edges.tolist()
 
-    for i in range(len(edges_binary)):
-        for j in range(len(edges_binary[0])):
+    num_cols = len(edges_binary[0])
+    for i in xrange(len(edges_binary)):
+        for j in xrange(num_cols):
             if edges_binary[i][j] > 0:
                 edges_binary[i][j] = 1
 
