3aea3c25e9e49124451184ff0465ca94e3b1b6c9
pijoules <lchan1994@yahoo.com>
2017-01-21 17:03:34 -0500

    gotta increase that github score

--- convert2d.py
+++ convert2d.py
@@ -4,7 +4,12 @@ from matplotlib import pyplot as plt
 from skimage import feature
 
 
-def getBinaryEdges(filename, sigma=0.33):
+def color_circle(img, x, y, r=2):
+    h, w = img.shape
+    img[max(0, y-2): min(h, y+2), max(0, x-2): min(w, x+2)] = 1
+
+
+def getBinaryEdges(filename, sigma=0.33, r=2):
     img = cv2.imread(filename, 0)
 
     assert img is not None, "Error imreading file '{}'".format(filename)
@@ -21,6 +26,14 @@ def getBinaryEdges(filename, sigma=0.33):
         for j in xrange(num_cols):
             if edges[i, j] > 0:
                 edges[i, j] = 1
+
+    # Expand edges
+    new_edges = np.zeros((len(edges), num_cols))
+    for y in xrange(len(edges)):
+        for x in xrange(num_cols):
+            if edges[y, x] > 0:
+                # Color areas around
+                color_circle(edges, x, y, r=r)
 
     return edges
 
